cbda2f761359689405f6ec84424b5a3a3f421bfb
m-rero <bws.rrodrigues@gmail.com>
2025-03-30 10:38:06 -0300

    first commit

---
+++ src/main/java/api/endeavorbackend/EndeavorBackendApplication.java
@@ -0,0 +1,13 @@
+package api.endeavorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndeavorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndeavorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/endeavorbackend/EndeavorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package api.endeavorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndeavorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
